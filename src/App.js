70280d207fb5e9024d3d182e8feb9f14d2f513a4
habibzaman <75401456+habibzaman@users.noreply.github.com>
2023-11-21 21:39:00 -0500

    test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+function DbSectoin(){
+  const [originalUrl, setOriginalUrl] = useState('');
+  const [shortenedUrl, setShortenedUrl] = useState('');
+
+  // const handleShortenClick = async () => {
+  //     const updateString = 'new update';
+  //     document.getElementById('shortenedUrl').innerHTML +="new text";
+  //     window.alert('button pressed');
+  // }
+
+    // const handleShortenClick = function(){
+    // console.log('button pressed');
+    // document.getElementById('shortenedUrl').innerHTML +="new text";
+    // window.alert('button pressed');
+// }
+    const handleShortenClick = (event) =>{
+      console.log(originalUrl);
+      event.preventDefault();
+      console.log('Button clicked');
+      window.alert('button clicked')
+      // setShortenedUrl('Random text');
+      // setShortenedUrl(originalUrl);
+
+      async function makeShortUrl () {
+        try{
+          // const response = await axios.get('../createShortUrl')
+          // .then((response)=>{
+          //   console.log(`response is: ${response}`);
+          //   setShortenedUrl(response);
+          // })
+          const response = await axios.post('http://localhost:3333/short', {
+            // method: 'post',
+            // url: 'https://localhost:3333',
+            origUrl: originalUrl,
+           })
+          const shortenedUrl = response.data.shortUrl; 
+          setShortenedUrl(shortenedUrl);
+          // console.log(`response is: ${response}`)
+          // setShortenedUrl(response);
+          // console.log(`shortened URL is: ${JSON.stringify(response)}`);
+          // }).then((response)=>{
+          //   console.log(`response is: ${response}`)
+          //   setShortenedUrl(response);
+          //   console.log(`shortened URL is: ${JSON.stringify(response)}`);
+          // });
+
+        } catch (error){
+          console.error('error is: ' + error)
+        }
+      } 
+      makeShortUrl();
+      
+    } 
+
+    return(  
+    <div className='container'>
+      <div className="container text-center">
+        <div className="row">
+          <div className="col">
+            <form>
+              <div className="mb-3">
+                <label htmlFor="originalUrl" className="form-label">Orignial URL</label>
+                <input 
+                type="text" 
+                className="form-control" 
+                onChange={(e) => setOriginalUrl(e.target.value)}
+                id="originalUrl" 
+                value={originalUrl} 
+                aria-describedby="urlHelp">
+                </input>
+                <div id="urlHelp" className="form-text">Enter your URL above!</div>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="shortenedUrl" 
+                className="form-label">Shortened URL:</label>
+                <input type="text" className="form-control" id="shortenedUrl" value={shortenedUrl} readOnly></input>
+              </div>
+              <button 
+              onClick={handleShortenClick} 
+              className="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>   
     </div>
-  );
+    )
+    
+}
+
+function App(){
+  return (
+    <div>
+      <DbSectoin />
+    </div>
+  )
 }
 
 export default App;
